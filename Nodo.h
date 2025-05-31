d388a7f33cf590e36463586ff4de1c6603ac8feb
Armoon01 <andy.luna.izaguirre@est.una.ac.cr>
2025-05-30 23:22:24 -0600

    Se agrego la clase Lista

---
+++ Nodo.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include "ObjectAdaptador.h"
+
+class Nodo : public ObjectAdaptador {
+private:
+	Nodo* _siguiente;
+	ObjectAdaptador* _dato;
+public:
+	Nodo(ObjectAdaptador*, Nodo* = NULL);
+	virtual void setSiguiente(Nodo*);
+	virtual void setDato(ObjectAdaptador*);
+	virtual Nodo* getSiguiente() const;
+	virtual ObjectAdaptador* getDato() const;
+};
+
